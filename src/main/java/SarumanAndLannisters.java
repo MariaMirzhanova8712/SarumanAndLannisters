e5cd029d8da7739d45c5d8abb0d93972d0c6a76e
Maria <mariamir8712@gmail.com>
2022-10-31 19:09:40 +0300

    first commit for the task SarumanAndLannisters

---
+++ src/main/java/SarumanAndLannisters.java
@@ -0,0 +1,15 @@
+public class SarumanAndLannisters {
+
+    public static void main(String args[]) {
+        int countOfInfantrymenLannisters = 100; // изменяемая переменная, количество человек может меняться
+        System.out.println("У Ланнистеров было " + countOfInfantrymenLannisters + " пехотинца(ев)." );
+
+        int countOfOrcsSaruman = 200; // изменяемая переменная, количество человек может меняться
+        System.out.println("У Сарумана было " + countOfOrcsSaruman + " орка(ов).");
+
+        int totalCountOfLannistersAndOrcsSaruman = countOfInfantrymenLannisters + countOfOrcsSaruman;
+        System.out.println("Всего пехоты в союзе Ланнистеров и Сарумана было " + totalCountOfLannistersAndOrcsSaruman + " человек(а).");
+
+    }
+
+}
